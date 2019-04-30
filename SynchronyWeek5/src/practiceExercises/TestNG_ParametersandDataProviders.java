644462cc453d92a442668efd063bc7636488843c
Airydin <samantha.mullenax92@gmail.com>
2019-04-30 09:34:11 -0400

    origin

---
+++ SynchronyWeek5/src/practiceExercises/TestNG_ParametersandDataProviders.java
@@ -0,0 +1,30 @@
+package practiceExercises;
+
+import org.testng.Assert;
+import org.testng.annotations.DataProvider;
+import org.testng.annotations.Test;
+
+public class TestNG_ParametersandDataProviders {
+ 
+	@Test(dataProvider = "ValidDataProvider")
+  public void CharToASCIITest(final char character, final int ascii) {
+		int result = CharUtils.CharToASCII(character);
+		Assert.assertEquals(result, ascii);
+	}//end at test 1
+	@Test(dataProvider = "ValidDataProvider")
+	public void ASCIIToCharTest(final char character, final int ascii) {
+		char result = CharUtils.ASCIIToChar(ascii);
+		Assert.assertEquals(result, character);
+	}
+	@DataProvider
+	public Object[][] ValidDataProvider() {
+		return new Object[][] {
+			{ 'A' , 65 },{'a' , 97},
+		    { 'B' , 66 },{'b' , 98},
+		    { 'C' , 67 },{'c' , 99},
+		    { 'D' , 68 },{'d' , 100},
+		    { 'Z' , 90 },{'z' , 122},
+		    { '1' , 49 },{'9' , 57},
+		};
+	}
+}
